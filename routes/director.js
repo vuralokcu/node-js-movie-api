bd019529064fb127cdff50772b2a7c5835fa93c8
Vural ÖKCÜ <vuralokcu@yandex.com>
2020-01-18 18:16:58 +0300

    /api/directors/:director_id (DELETE) method completed.

--- routes/director.js
+++ routes/director.js
@@ -147,4 +147,40 @@ router.get('/:director_id', (req, res, next) => {
     });
 });
 
+
+// Update director
+router.put('/:director_id', (req, res, next) => {
+    const director_id = req.params.director_id;
+    const promise = Director.findByIdAndUpdate(
+        director_id,
+        req.body,
+        {
+            new: true // Sonuç olarak dönen data eskisi değil, yenisi olsun istiyorsak kullanılır.
+        }
+    );
+
+    promise.then((director) => {
+        if (! director)
+            return next({ message: 'The director was not found.', code: 99 });
+
+        res.json(director);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+
+// director deleting
+router.delete('/:director_id', (req, res, next) => {
+    const promise = Director.findByIdAndRemove(req.params.director_id);
+
+    promise.then((director) => {
+        if (! director)
+            return next({ message: 'The director was not found.', code: 99 });
+        res.json({ result: true })
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
 module.exports = router;

52164633cb175d82ed8a1e4c5169f0c2b78f6099
Vural ÖKCÜ <vuralokcu@yandex.com>
2020-01-19 02:50:41 +0300

    Director and Movie model validation updated.

--- app/models/Director.js
+++ app/models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        maxlength: 60,
+        minlength: 2
+    },
+    surname: {
+        type: String,
+        maxlength: 60,
+        minlength: 2
+    },
+    bio: {
+        type: String,
+        maxlength: 10000,
+        minlength: 60
+    },
     created_at: {
         type: Date,
         default: Date.now
